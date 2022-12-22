bd854f6d190f8fcedf9e3271621d07350e1ce6de
Yibo Long <yibo@span.io>
2022-12-21 18:19:30 -0800

    Fix errors

---
+++ src/scripts/install-jdk.sh
@@ -0,0 +1,3 @@
+sudo apt-get update && sudo apt-get install java-common
+wget https://corretto.aws/downloads/latest/amazon-corretto-"$JDK_VERSION"-x64-linux-jdk.deb
+sudo dpkg --install amazon-corretto-"$JDK_VERSION"-x64-linux-jdk.deb
--- src/scripts/intall-jdk.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update && sudo apt-get install java-common
-wget https://corretto.aws/downloads/latest/amazon-corretto-$JDK_VERSION-x64-linux-jdk.deb
-sudo dpkg --install amazon-corretto-$JDK_VERSION-x64-linux-jdk.deb
